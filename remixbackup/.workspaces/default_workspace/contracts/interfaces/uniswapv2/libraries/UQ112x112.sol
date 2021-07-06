6df2d4e87cde9a2e48ad0c51af264139302c354a
Zycl0n <rahilwijesooriya@gmail.com>
2021-07-06 12:37:51 +0530

    Initial Commit

---
+++ remixbackup/.workspaces/default_workspace/contracts/interfaces/uniswapv2/libraries/UQ112x112.sol
@@ -0,0 +1,9 @@
+pragma solidity =0.6.12;
+
+// a library for handling binary fixed point numbers (https://en.wikipedia.org/wiki/Q_(number_format))
+
+// range: [0, 2**112 - 1]
+// resolution: 1 / 2**112
+
+library UQ112x112 {
+    uint256 constant Q112 = 2**112;
